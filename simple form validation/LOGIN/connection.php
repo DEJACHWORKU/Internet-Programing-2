38f862f2e8d498ebeee3937be6c2074216b08f5b
DEJACHWORKU <dejachworkuit@gmail.com>
2024-11-02 13:04:17 +0300

    Create connection.php

---
+++ simple form validation/LOGIN/connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost"; // Change if your server is different
+$username = "your_username"; // Your database username
+$password = "your_password"; // Your database password
+$dbname = "your_database"; // Your database name
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
