82e3e3828eeaec2d8e6f8338a022b73ea4b0a0d1
DEJACHWORKU <dejachworkuit@gmail.com>
2024-11-02 13:05:28 +0300

    Update index.php

--- simple form validation/LOGIN/index.php
+++ simple form validation/LOGIN/index.php
@@ -6,14 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LOGIN</title>
     <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-    <style>
-        .error {
-            color: red;
-            font-size: 0.9em;
-            margin-top: 5px;
-        }
-    </style>
 </head>
 <body>
     <form action="login.php" method="post">
